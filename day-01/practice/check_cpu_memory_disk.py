9c50daae8889fc6e76aa50cb6da8e3974af9124b
dsusmit63 <dsusmit63@gmail.com>
2025-12-22 11:30:29 +0530

    updated check_cpu_memory_disk.py

--- day-01/practice/check_cpu_memory_disk.py
+++ day-01/practice/check_cpu_memory_disk.py
@@ -1,13 +1,12 @@
 import psutil
 cpu_threshold = int(input("Enter CPU threshold: "))
 mem_threshold = int(input("Enter memory threshold: "))
-disk_threshold =int(input("Enter disk threshold: ")) 
+disk_threshold = int(input("Enter disk threshold: ")) 
 
 for i in range(5):
   def check_cpu_memory_disk(cpu_threshold,mem_threshold,disk_threshold):
     cpu_usage = psutil.cpu_percent(interval=1)
     virtual_mem_usage = psutil.virtual_memory().percent
-    swap_mem_usage = psutil.swap_memory().percent
     disk_usage = psutil.disk_usage('/').percent
 
     if(cpu_usage > cpu_threshold):
